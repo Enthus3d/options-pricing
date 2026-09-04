8864cb8889724479cc5db46a7f4fbccb387a6f49
Enthus3d <manavdesaim123@gmail.com>
2026-09-04 11:26:31 +0100

    Added functions for antithetic variates calculations

--- monte_carlo/pricer.py
+++ monte_carlo/pricer.py
@@ -10,13 +10,13 @@ class InvalidOptionType(Exception):
     def __str__(self):
         return f"{self.message} (Error code: {self.error_code})"
 
-def simulate_S_T(S, r, sigma, T, Z_n):
+def simulate_S_T(S, T, r, sigma, Z_n):
     return S*np.exp(T*(r-(0.5*(sigma**2)))+(sigma*np.sqrt(T)*Z_n))
 
 def generate_normals(n):
     return np.random.normal(0.0, 1.0, n)
 
-def payoff(S_T_n, K, option_type):
+def payoff_0(S_T_n, K, option_type):
     if option_type == "call":
         return np.maximum(0,S_T_n-K)
     elif option_type == "put":
@@ -24,16 +24,29 @@ def payoff(S_T_n, K, option_type):
     else:
         raise InvalidOptionType(f"Invalid option type \"{option_type}\" for options \"put\" or \"call\"", 400)
 
-def mc_price(S, K, r, sigma, T, n, option_type):
+def payoff(S, T, K, r, sigma, Z_n, option_type):
+    return payoff_0(simulate_S_T(S, T, r, sigma, Z_n), K, option_type)
+
+def mc_price(S, T, K, r, sigma, n, option_type):
     Z_n = generate_normals(n)
-    payoffs = payoff(simulate_S_T(S, r, sigma, T, Z_n), K, option_type)
+    payoffs = payoff(S, T, K, r, sigma, Z_n, option_type)
     price = np.exp(-r*T)*np.mean(payoffs)
     return price
 
-def mc_standard_error(payoffs, r, T):
+def mc_price_from_payoff(T, r, payoffs):
+    return np.exp(-r*T)*np.mean(payoffs)
+
+def mc_standard_error(payoffs, T, r):
     return (np.std(payoffs, ddof = 1)*np.exp(-r*T))/np.sqrt(len(payoffs))
 
 def confidence_interval(price, standard_error, level = 0.95):
     critical_value = stats.norm.ppf(1-(1-level)/2)
     return (price - (standard_error*critical_value), price + (standard_error*critical_value))
 
+def convergence_sweep(S, T, K, r, sigma, n_vals, option_type):
+    prices_se = []
+    for i in n_vals:
+        Z_n = generate_normals(i)
+        payoffs = payoff(S, T, K, r, sigma, Z_n, option_type)
+        prices_se.append([mc_price_from_payoff(T, r, payoffs), payoffs])
+    return prices_se
--- monte_carlo/variance_reduction.py
+++ monte_carlo/variance_reduction.py
@@ -1,9 +1,27 @@
-import options-pricer.monte_carlo 
+import options-pricer.monte_carlo.pricer
 
 def generate_antithetic_normals(n):
     half_n = math.floor(n/2)
-    positive_normal = monte_carlo.generate_normals(half_n)
+    positive_normal = pricer.generate_normals(half_n)
     negative_normal = -positive_normal
-    return np.concatenate(negative_normal, positive_normal)
+    return [negative_normal, positive_normal]
 
+def mc_antithetic_price(S, T, K, r, sigma, n, option_type):
+    Z_n = generate_antithetic_normals(n)
+    for i in range(2):
+        Z_n[i] = pricer.payoff(S, T, K, r, sigma, Z_n[i], option_type)
+    Y = 0.5*(Z_n[0]+Z_n[1])
+    price = np.exp(-r*T)*np.mean(Y)
+    return price
+
+def antithetic_payoff(S, T, K, r, sigma, Z_n, option_type):
+    for i in range(2):
+        Z_n[i] = pricer.payoff(S, T, K, r, sigma, Z_n[i], option_type)
+    return Z_n
+ 
+
+def antithetic_standard_error(payoffs, r, T):
+    Y = 0.5*(payoffs[0]+payoffs[1])
+    return pricer.mc_standard_error(Y, T, r)
+        
 
