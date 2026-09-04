259c71d5475940750da126c1cae8bb280ec6f9a8
Enthus3d <manavdesaim123@gmail.com>
2026-09-04 18:35:06 +0100

    Investigated rate of convergence of the simlutaion to the analytical solution, added VRF analysis

--- monte_carlo/variance_reduction.py
+++ monte_carlo/variance_reduction.py
@@ -10,7 +10,7 @@ def generate_antithetic_normals(n):
 
 def antithetic_payoff(S, K, T, r, sigma, Z_n, option_type):
     for i in range(2):
-        Z_n[i] = pricer.payoff(S, T, K, r, sigma, Z_n[i], option_type)
+        Z_n[i] = pricer.payoff(S, K, T, r, sigma, Z_n[i], option_type)
     return Z_n
 
 def mc_antithetic_price(S, K, T, r, sigma, n, option_type):
@@ -25,10 +25,10 @@ def antithetic_standard_error(payoffs, r, T):
     Y = 0.5*(payoffs[0]+payoffs[1])
     return pricer.mc_standard_error(Y, T, r)
         
-def variance_reduction_factor(S, K, T, r, sigma, n, n_repeats, option_type):
+def variance_reduction_factor(n, n_repeats, S, K, T, r, sigma, option_type):
     p = []
     ap = []
     for i in range(n_repeats):
         p.append(pricer.mc_price(S, K, T, r, sigma, n, option_type))
         ap.append(mc_antithetic_price(S, K, T, r, sigma, n, option_type))
-    return np.var(p, ddof = 1)/np.var(ap, ddof = 1)
+    return [np.var(p, ddof = 1)/np.var(ap, ddof = 1), p, ap]
